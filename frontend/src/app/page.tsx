f0288a98628b2b358dacb205ea9aef46209b8f3b
Zach Wilson <zachwilson130@gmail.com>
2024-09-08 22:21:29 +0000

    adding buttons for quick github

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -26,66 +26,81 @@ const GithubUrlForm = () => {
   };
 
   return (
-    <div className="flex flex-col items-center gap-4 w-full max-w-lg p-3">
-      <img src="/logo.png" alt="changelog++" className="w-full" />
-      <form
-        onSubmit={handleSubmit}
-        className="flex flex-col items-center gap-4 w-full"
-      >
-        <div className="flex flex-col gap-2">
-          <p className="text-2xl font-bold text-gray-800 dark:text-gray-200">
-            I want a changelog for
-            <input
-              type="url"
-              value={url}
-              onChange={(e) => setUrl(e.target.value)}
-              placeholder="GitHub link"
-              className="px-4 py-2 w-48 mx-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-black dark:text-white dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600"
-              required
-            />
-            from the last
-            <input
-              type="number"
-              value={days}
-              onChange={(e) => setDays(parseInt(e.target.value))}
-              placeholder="Enter days"
-              className="px-4 py-2 border border-gray-300 rounded-md w-16 mx-2 focus:outline-none focus:ring-2 focus:ring-blue-500 text-black dark:text-white dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600"
-              required
-            />
-            days.
-          </p>
-        </div>
+      <div className="flex flex-col items-center gap-4 w-full max-w-lg p-3">
+        <img src="/logo.png" alt="changelog++" className="w-full"/>
 
-        <button
-          type="submit"
-          className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-        >
-          Generate Changelog
-        </button>
-      </form>
-      {isLoading && <p className="dark:text-white">Loading...</p>}
-      {changelog && (
-        <div className="dark:text-gray-400 prose prose-sm dark:prose-invert">
-          <ReactMarkdown
-            components={{
-              img: function (props) {
-                return (
-                  <img {...props} style={{ width: "50px", height: "50px" }} />
-                );
-              },
-            }}
+
+          <div className={'flex gap-3'}>
+              <button className='btn-primary bg-green-600 p-5 rounded-2xl'
+                      onClick={() => setUrl("https://github.com/vercel/next.js")}>NextJS
+              </button>
+              <button className='btn-primary bg-green-600 p-5 rounded-2xl'
+                      onClick={() => setUrl("https://github.com/facebook/react")}>ReactJS
+              </button>
+
+              <button className='btn-primary bg-green-600 p-5 rounded-2xl'
+                      onClick={() => setUrl("https://github.com/meta-llama/llama")}>Llama
+              </button>
+          </div>
+
+          <form
+              onSubmit={handleSubmit}
+              className="flex flex-col items-center gap-4 w-full"
           >
-            {changelog}
-          </ReactMarkdown>
-        </div>
-      )}
-    </div>
+              <div className="flex flex-col gap-2">
+                  <p className="text-2xl font-bold text-gray-800 dark:text-gray-200">
+                      I want a changelog for
+                      <input
+                          type="url"
+                  value={url}
+                  onChange={(e) => setUrl(e.target.value)}
+                  placeholder="GitHub link"
+                  className="px-4 py-2 w-48 mx-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-black dark:text-white dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600"
+                  required
+              />
+              from the last
+              <input
+                  type="number"
+                  value={days}
+                  onChange={(e) => setDays(parseInt(e.target.value))}
+                  placeholder="Enter days"
+                  className="px-4 py-2 border border-gray-300 rounded-md w-16 mx-2 focus:outline-none focus:ring-2 focus:ring-blue-500 text-black dark:text-white dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600"
+                  required
+              />
+              days.
+            </p>
+          </div>
+
+          <button
+              type="submit"
+              className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+          >
+            Generate Changelog
+          </button>
+        </form>
+        {isLoading && <p className="dark:text-white">Loading...</p>}
+        {changelog && (
+            <div className="dark:text-gray-400 prose prose-sm dark:prose-invert">
+              <ReactMarkdown
+                  components={{
+                    img: function (props) {
+                      return (
+                          <img {...props} style={{width: "50px", height: "50px"}}/>
+                      );
+                    },
+                  }}
+              >
+                {changelog}
+              </ReactMarkdown>
+            </div>
+        )}
+      </div>
   );
 };
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-12">
+      <div className="flex flex-col items-center justify-center min-h-screen py-12">
       <GithubUrlForm />
     </div>
   );

ed5be385e3dd0d07fa5db47b99e445a7feeae7af
Zach Wilson <zachwilson130@gmail.com>
2024-09-08 18:49:15 +0000

    adding profile pictures for the developers

--- frontend/src/app/backendInFrontend.ts
+++ frontend/src/app/backendInFrontend.ts
@@ -1,167 +1,179 @@
 import axios from "axios";
 
 interface Commit {
-  stats: {
-    total: number;
-    additions: number;
-    deletions: number;
-  };
-  files: {
-    filename: string;
-    patch: string;
-  }[];
+    author: {
+        avatar_url: string,
+        login: string
+    },
+    commit: {
+        author: {
+            email: string,
+            name: string
+        }
+    },
+    stats: {
+        total: number;
+        additions: number;
+        deletions: number;
+    };
+    files: {
+        filename: string;
+        patch: string;
+    }[];
 }
 
 type SomeType = {
-  url: string;
+    url: string;
 };
 
 async function getRepoDiff(
-  repoUrl: string,
-  days: number
+    repoUrl: string,
+    days: number
 ): Promise<{
-  additions: number;
-  deletions: number;
-  netDiff: number;
-  diffs: string[];
+    additions: number;
+    deletions: number;
+    netDiff: number;
+    diffs: string[];
 }> {
-  const [owner, repo] = repoUrl.split("/").slice(-2);
-  const apiUrl = `https://api.github.com/repos/${owner}/${repo}/commits`;
+    const [owner, repo] = repoUrl.split("/").slice(-2);
+    const apiUrl = `https://api.github.com/repos/${owner}/${repo}/commits`;
 
-  const since = new Date();
-  since.setDate(since.getDate() - days);
+    const since = new Date();
+    since.setDate(since.getDate() - days);
 
-  try {
-    const githubPAT = process.env.NEXT_PUBLIC_GITHUB_PAT;
-    if (!githubPAT) {
-      throw new Error("GitHub Personal Access Token is missing");
-    }
+    try {
+        const githubPAT = process.env.NEXT_PUBLIC_GITHUB_PAT;
+        if (!githubPAT) {
+            throw new Error("GitHub Personal Access Token is missing");
+        }
 
-    const response = await axios.get(apiUrl, {
-      params: {
-        since: since.toISOString(),
-      },
-      headers: {
-        Accept: "application/vnd.github.v3+json",
-        Authorization: `token ${githubPAT}`,
-      },
-    });
-
-    const commits: Commit[] = await Promise.all(
-      response.data.map((commit: SomeType) =>
-        axios
-          .get(commit.url, {
-            headers: {
-              Accept: "application/vnd.github.v3+json",
-              Authorization: `token ${githubPAT}`,
+        const response = await axios.get(apiUrl, {
+            params: {
+                since: since.toISOString(),
             },
-          })
-          .then((res) => res.data)
-      )
-    );
+            headers: {
+                Accept: "application/vnd.github.v3+json",
+                Authorization: `token ${githubPAT}`,
+            },
+        });
 
-    const totals = commits.reduce(
-      (acc, commit) => {
-        acc.additions += commit.stats.additions;
-        acc.deletions += commit.stats.deletions;
-        return acc;
-      },
-      { additions: 0, deletions: 0 }
-    );
+        const commits: Commit[] = await Promise.all(
+            response.data.map((commit: SomeType) =>
+                axios
+                    .get(commit.url, {
+                        headers: {
+                            Accept: "application/vnd.github.v3+json",
+                            Authorization: `token ${githubPAT}`,
+                        },
+                    })
+                    .then((res) => res.data)
+            )
+        );
 
-    const netDiff = totals.additions - totals.deletions;
+        const totals = commits.reduce(
+            (acc, commit) => {
+                acc.additions += commit.stats.additions;
+                acc.deletions += commit.stats.deletions;
+                return acc;
+            },
+            {additions: 0, deletions: 0}
+        );
 
-    const diffs = commits.flatMap((commit) =>
-      commit.files.map((file) => `File: ${file.filename}\n${file.patch}`)
-    );
+        const netDiff = totals.additions - totals.deletions;
 
-    return {
-      additions: totals.additions,
-      deletions: totals.deletions,
-      netDiff,
-      diffs,
-    };
-  } catch (error) {
-    console.error("Error fetching repository data:", error);
-    throw error;
-  }
+        const diffs = commits.flatMap((commit) =>
+            commit.files.map((file) => `Author: ${commit.author.login}. Author Profile Picture URL: ${commit.author.avatar_url}. File: ${file.filename}\n${file.patch}`)
+        );
+
+        return {
+            additions: totals.additions,
+            deletions: totals.deletions,
+            netDiff,
+            diffs,
+        };
+    } catch (error) {
+        console.error("Error fetching repository data:", error);
+        throw error;
+    }
 }
 
-async function generateChangelog(diffs: string[], repo: string, owner:string): Promise<string> {
-  const greptileApiKey = process.env.NEXT_PUBLIC_GREPTILE_API_KEY;
-  const githubToken = process.env.NEXT_PUBLIC_GITHUB_PAT;
+async function generateChangelog(diffs: string[], repo: string, owner: string): Promise<string> {
+    const greptileApiKey = process.env.NEXT_PUBLIC_GREPTILE_API_KEY;
+    const githubToken = process.env.NEXT_PUBLIC_GITHUB_PAT;
 
-  if (!greptileApiKey) {
-    throw new Error("Greptile API key is missing");
-  }
-
-  if (!githubToken) {
-    throw new Error("GitHub token is missing");
-  }
-
-  try {
-    const queryPayload = {
-      messages: [
-        {
-          content: `Generate a concise changelog based on the following diffs: ${diffs.join(
-            "\n\n"
-          )}`,
-          role: "user",
-        },
-      ],
-      repositories: [
-        {
-          remote: "github",
-          repository: `${owner}/${repo}`,
-          branch: "main",
-        },
-      ],
-      genius: true,
-    };
-
-    const changelogResponse = await fetch("https://api.greptile.com/v2/query", {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${greptileApiKey}`,
-        "X-Github-Token": githubToken,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(queryPayload),
-    });
-
-    if (!changelogResponse.ok) {
-      console.error("Response:", changelogResponse);
-      throw new Error(
-        `Failed to generate changelog: ${changelogResponse.statusText}`
-      );
+    if (!greptileApiKey) {
+        throw new Error("Greptile API key is missing");
     }
-    const changelogData = await changelogResponse.json();
-    return changelogData.message;
-  } catch (error) {
-    console.error("Error generating changelog:", error);
-    throw error;
-  }
+
+    if (!githubToken) {
+        throw new Error("GitHub token is missing");
+    }
+
+    try {
+        const queryPayload = {
+            messages: [
+                {
+                    content: `Generate a concise changelog based on the following diffs. 
+                              Make sure to group the changes by each developer with their profile picture in markdown: ${diffs.join(
+                        "\n\n"
+                    )}`,
+                    role: "user",
+                },
+            ],
+            repositories: [
+                {
+                    remote: "github",
+                    repository: `${owner}/${repo}`,
+                    branch: "main",
+                },
+            ],
+            genius: true,
+        };
+
+        const changelogResponse = await fetch("https://api.greptile.com/v2/query", {
+            method: "POST",
+            headers: {
+                Authorization: `Bearer ${greptileApiKey}`,
+                "X-Github-Token": githubToken,
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(queryPayload),
+        });
+
+        if (!changelogResponse.ok) {
+            console.error("Response:", changelogResponse);
+            throw new Error(
+                `Failed to generate changelog: ${changelogResponse.statusText}`
+            );
+        }
+        const changelogData = await changelogResponse.json();
+        console.log(changelogData);
+        return changelogData.message;
+    } catch (error) {
+        console.error("Error generating changelog:", error);
+        throw error;
+    }
 }
 
 export async function generateChangelogForGreptileDocs(url: string) {
 
-  const tokens = url.split('/');
-  const repo = tokens.pop();
-  const owner = tokens.pop();
+    const tokens = url.split('/');
+    const repo = tokens.pop();
+    const owner = tokens.pop();
 
-  if(!repo || !owner){
-    return;
-  }
+    if (!repo || !owner) {
+        return;
+    }
 
-  try {
-    const repoDiffResult = await getRepoDiff(
-      `https://github.com/${owner}/${repo}`,
-      30
-    );
-    console.log(repoDiffResult.diffs);
-    const changelog = await generateChangelog(repoDiffResult.diffs, repo, owner);
-    return changelog;
-  } catch (error) {
-    console.error("Error generating changelog for greptileai/docs:", error);
-  }
+    try {
+        const repoDiffResult = await getRepoDiff(
+            `https://github.com/${owner}/${repo}`,
+            7
+        );
+        console.log(repoDiffResult.diffs);
+        const changelog = await generateChangelog(repoDiffResult.diffs, repo, owner);
+        return changelog;
+    } catch (error) {
+        console.error("Error generating changelog for greptileai/docs:", error);
+    }
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -42,7 +42,7 @@ const GithubUrlForm = () => {
       </form>
       {isLoading && <p className="dark:text-white">Loading...</p>}
       {changelog && (
-        <div className="dark:text-white prose prose-sm">
+        <div className="dark:text-gray-400 prose prose-sm">
           <ReactMarkdown>{changelog}</ReactMarkdown>
         </div>
       )}
